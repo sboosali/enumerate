93b5049f247b83170465e411ff9d7d53f4d9578d
Sam Boosalis <samboosalis@gmail.com>
2015-10-12 20:53:31 -0700

    (incomplete) Data.CoRec.MemoTrie

--- sources/Data/CoRec/MemoTrie.hs
+++ sources/Data/CoRec/MemoTrie.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, DeriveDataTypeable, LambdaCase, TypeFamilies, UndecidableInstances, ScopedTypeVariables, FlexibleContexts  #-}
+{-# LANGUAGE TypeOperators, DeriveDataTypeable, LambdaCase, TypeFamilies, UndecidableInstances, ScopedTypeVariables, FlexibleContexts, GADTs, ConstraintKinds, DataKinds #-}
 
 {-| 
 
@@ -12,20 +12,22 @@ import Data.Vinyl.TypeLevel
 import Data.Vinyl.Functor
 
 import           GHC.Generics
+import GHC.Prim (Constraint)
+import Data.Proxy (Proxy(..)) 
 
 
 {-| 
 
 -}
 instance (RecAll f as HasTrie, RecApplicative as) => HasTrie (CoRec f as) where
- -- newtype (CoRec f as) :->: b = CoRecTrie { unCoRecTrie :: (Rec ((:<-:) b :. (Dict HasTrie)) as) }
  newtype (CoRec f as) :->: b = CoRecTrie { unCoRecTrie :: (Rec ((:<-:) b :. f) as) }
- trie   = CoRecTrie . trieCoRec 
- untrie = untrieCoRec . unCoRecTrie
+ trie   = undefined -- CoRecTrie . trieCoRec 
+ untrie = undefined -- untrieCoRec . unCoRecTrie
 
 newtype (b :<-: a) = OpTrie (a :->: b)
 
--- reifyConstraint (Proxy :: Proxy HasTrie) 
+-- | a flipped '∈'. 
+type (∋) rs r = RElem r rs (RIndex r rs)
 
 {-| 
 
@@ -35,40 +37,55 @@ generalizes:
 
 @
 instance (HasTrie a, HasTrie b) => HasTrie (Either a b) where
-    trie f = 'EitherTrie' (trie (f . Left)) (trie (f . Right))
+  trie f = 'EitherTrie' (trie (f . Left)) (trie (f . Right))
+  ...
 @
 
 
 -}
-trieCoRec :: forall f as b. (RecAll f as HasTrie, RecApplicative as) => (CoRec f as -> b) -> (Rec ((:<-:) b :. f) as)
--- trieCoRec f = rpure (colTrie f)
-trieCoRec f = rpure oneTrie 
- where
- -- oneTrie :: forall a. (a ∈ as) => (b :<-: (Dict HasTrie (f a)))
- -- oneTrie :: (b :<-: (Dict HasTrie (f a)))
- -- oneTrie :: forall a. (b :<-: (Dict HasTrie (f a)))
- oneTrie :: forall a. (a ∈ as, HasTrie (f a)) => ((:<-:) b :. f) a
- oneTrie = (colTrie (f . Col))
+-- trieCoRec :: forall f as b. (RecAll f as HasTrie, RecApplicative as) => (CoRec f as -> b) -> (Rec ((:<-:) b :. f) as)
+-- trieCoRec f = rpure (Compose (OpTrie (trie (f . Col))))
+--  where
 
--- type ElemHasTrie f as a = ((a ∈ as), (HasTrie (f a)))
+-- example = reifyConstraints (Proxy :: Proxy ((∋) rs)) (Proxy :: Proxy HasTrie) 
 
--- {-| 
+{-| 
+
+-}
+-- untrieCoRec :: (RecAll f as HasTrie) => (Rec ((:<-:) b) as) -> (CoRec f as -> b)
+-- untrieCoRec tries = f
+--  where
+--  f = undefined
+
+{-| like 'Dict', but can reify constraints on the raw types as well as the items.
+
+-}
+data Dicts cType cItem f a where
+  Dicts
+    :: (cType a, cItem (f a))
+    => !(f a)
+    -> Dicts cType cItem f a
+
+{-| A constraint on each type in a type-level list.
+
+-}
+type family EachHas ts c :: Constraint where
+  EachHas '[] c = ()
+  EachHas (t ': ts) c = (c t, EachHas ts c)
+
+-- {-| like 'reifyConstraint', but can reify constraints on the raw types as well as the items.
+
+-- e.g. given @Rec Maybe [Bool,String]@, it can reify @(cType Bool, cType String, cItem (Maybe Bool), cItem (Maybe String))@. 
+
 
 -- -}
--- colTrie :: ((Dict HasTrie (f a)) -> b) -> (b :<-: (Dict HasTrie (f a)))
--- colTrie f = (OpTrie (trie f))
-
--- colTrie :: forall f a b. (HasTrie (f a)) => (f a -> b) -> (b :<-: f a)
--- colTrie f = (OpTrie (trie f))
-
-colTrie :: forall f a b. (HasTrie (f a)) => (f a -> b) -> ((:<-:) b :. f) a
-colTrie f = Compose (OpTrie (trie f))
-
--- getDict :: c a => Dict c a -> a
--- getDict (Dict a) = a
-
--- untrieCoRec :: (RecAll f as HasTrie) => (Rec ((:<-:) b) as) -> (CoRec f as -> b)
-untrieCoRec tries = f
- where
- f = undefined 
+-- reifyConstraints
+--   :: (EachHas as cType, RecAll f as cItem)
+--   => proxy cType
+--   -> proxy cItem
+--   -> Rec f as
+--   -> Rec (Dicts cType cItem f :. f) as
+-- reifyConstraints cType cItem = \case 
+--     RNil -> RNil
+--     (x :& xs) -> Compose (Dicts x) :& reifyConstraints cType cItem xs 
 
