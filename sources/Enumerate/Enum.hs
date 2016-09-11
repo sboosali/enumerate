d6edf43fe77baf0f9ce024b0ade0b58cfc445b4a
Lingnan Dai <lingnan.d@gmail.com>
2016-09-11 16:47:27 +0100

    add {toEnum,fromEnum}Default

--- sources/Enumerate/Enum.hs
+++ sources/Enumerate/Enum.hs
@@ -43,6 +43,9 @@ module Enumerate.Enum
 
  , array_enumerable
  , table_enumerable
+
+ , toEnumDefault
+ , fromEnumDefault
  ) where
 
 import Enumerate.Types
@@ -84,6 +87,11 @@ table_enumerable :: forall a. (Enumerable a, Ord a) => Map a Int
 table_enumerable = Map.fromList (zip enumerated [0 .. n - 1])
  where n = nat2int $ cardinality ([] :: [a])
 
+toEnumDefault :: forall a. (Enumerable a) => Int -> a
+toEnumDefault = toEnum_enumerable array_enumerable
+
+fromEnumDefault :: forall a. (Enumerable a, Ord a) => a -> Int
+fromEnumDefault = fromEnum_enumerable table_enumerable
 
 __fromJust__ :: String -> Maybe a -> a
 __fromJust__ name = maybe (__bug__ name) id
