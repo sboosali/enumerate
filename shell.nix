cb2ae12c6646b3d47d9edee45e1577aa05b65cbd
Spiros Boosalis <SamBoosalis@gmail.com>
2016-12-31 22:19:10 +0000

    shell.nix with cabal-install buildTools

--- shell.nix
+++ shell.nix
@@ -1,39 +1,80 @@
+
+/*
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  local = { 
+  f = import ./default.nix;
+  ps = {
     spiros = ../spiros;
   };
-
-  f = { mkDerivation, array, base, containers, deepseq, doctest
-      , ghc-prim, spiros, stdenv, template-haskell, vinyl
-      }:
-      mkDerivation {
-        pname = "enumerate";
-        version = "0.2.1";
-        sha256 = "0";
-        isLibrary = true;
-        isExecutable = true;
-        libraryHaskellDepends = [
-          array base containers deepseq ghc-prim spiros template-haskell
-          vinyl
-        ];
-        executableHaskellDepends = [ base ];
-        testHaskellDepends = [ base doctest ];
-        homepage = "https://github.com/sboosali/enumerate";
-        description = "enumerate all the values in a finite type (automatically)";
-        license = stdenv.lib.licenses.mit;
-      };
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f local;
+#  drv = pkgs.stdenv.lib.overrideDerivation drv0 (as: is);
+#  drv = drv0.overrideDerivation (as: is); 
+#  drv = pkgs.haskell.lib.addBuildTool drv0 (with pkgs; [cabal-install] );
+  drv = drv0;
+  drv0 = haskellPackages.callPackage f ps;
+
+  is = {
+    buildTools = with pkgs; [cabal-install];
+  };
 
 in
 
   if pkgs.lib.inNixShell then drv.env else drv
+
+*/
+
+
+/*
+with (import <nixpkgs> {}).pkgs;
+let
+
+myHaskellPackages = haskellPackages.override {
+ overrides = self: super: {
+   spiros    = self.callPackage ../spiros {};
+   enumerate = self.callPackage ./. {};
+   };
+ };
+
+ in
+
+myHaskellPackages.enumerate.env
+
+*/
+
+{ nixpkgs ? import <nixpkgs> {}
+, compiler ? "default" 
+}:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  f = import ./.;
+
+  ps = {
+    spiros = ../spiros;
+  };
+
+  d0 = haskellPackages.callPackage f ps;
+  d = (pkgs.haskell.lib.addBuildTools d0 [
+        haskellPackages.cabal-install
+        haskellPackages.hlint
+      ]);
+
+  e = if pkgs.lib.inNixShell then d.env else d;
+
+in 
+
+e
