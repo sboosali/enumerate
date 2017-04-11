6ea05036678751c7c76299f259fa974c671caa4a
Spiros Boosalis <SamBoosalis@gmail.com>
2017-04-11 01:24:13 +0000

    [bad] nix

--- default.nix
+++ default.nix
@@ -1,24 +1,12 @@
-{ ps ? (import <nixpkgs> {}).pkgs, mkDerivation
-, array, base, containers, deepseq, doctest, ghc-prim
-, spiros, stdenv, template-haskell, vinyl
-}:
-mkDerivation {
-  pname = "enumerate";
-  version = "0.2.1";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  libraryHaskellDepends = [
-    array base containers deepseq ghc-prim spiros template-haskell
-    vinyl
-  ];
-  executableHaskellDepends = [ base ];
-  testHaskellDepends = [ base doctest ];
-  homepage = "https://github.com/sboosali/enumerate";
-  description = "enumerate all the values in a finite type (automatically)";
-  license = stdenv.lib.licenses.mit;
-  # shellHook = '' '';
-  # buildTools = (with ps; [cabal-install haskellPackages.stack]);  
-  enableSplitObjs = false;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
+let
 
+h = nixpkgs.pkgs.haskell.packages.${compiler};
+
+ps = {
+ spiros = h.callPackage ~/spiros {}; # absolute
+};
+
+in
+
+h.callPackage ./package.nix ps
