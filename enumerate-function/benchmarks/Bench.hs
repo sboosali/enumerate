3aaa175c5173becf16ffc32940e480c687b50698
Sam Boosalis <SamBoosalis@gmail.com>
2018-12-03 08:55:54 -0800

    [enumerate-function] fix tests and benches

--- enumerate-function/benchmarks/Bench.hs
+++ enumerate-function/benchmarks/Bench.hs
@@ -1,10 +1,11 @@
 import Enumerate()
 import Criterion.Main
+import Prelude
 
 main = defaultMain [
   bgroup "Enumerate"
-    [ bench "1" $ nf   length [1..1000::Int]
-    , bench "2" $ whnf length [1..1000::Int]
+    [ bench "1" $ nf   length [1 .. 1000::Int]
+    , bench "2" $ whnf length [1 .. 1000::Int]
     ]
   ]
 
